d0c27f64f9031fec27f0da5993f0585e992f731c
l4yn3 <aa@gmail.com>
2025-06-14 18:58:04 +0800

    fix: 修复部分无法启动的bug

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from app import create_app,scheduler
-from flask_script import Manager, Server
+#from flask_script import Manager, Server
 
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
@@ -9,8 +9,9 @@ from geventwebsocket.handler import WebSocketHandler
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 #manager = Manager(app)
 
-server = pywsgi.WSGIServer(listener=("0.0.0.0",5000), application=app, handler_class=WebSocketHandler)
+server = pywsgi.WSGIServer(listener=("0.0.0.0",6000), application=app, handler_class=WebSocketHandler)
 #manager.add_command("runserver", server)
+application = app
 
 
 if __name__ == '__main__':
